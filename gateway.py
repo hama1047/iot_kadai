3293b3e85c6726248d10e2c3d9979b379f086ee7
hamada <hama1047@gmail.com>
2018-05-12 09:07:57 +0900

    jsde

--- gateway.py
+++ gateway.py
@@ -2,12 +2,13 @@ import serial
 import requests
 
 ser = serial.Serial('/dev/ttyUSB0',9600,timeout=None)
-url = "http://localhost:3000"
+url = "http://160.16.210.86:15071/putdata"
 
 while True:
     line = ser.readline()
-    lux = line.strip().decode('utf-8')
-    requests.get(url + "?lux=" + lux)
-    print(lux)
+    data = line.strip().decode('utf-8')
+    data = data.strip().split(',');
+    requests.get(url + "?lux=" + data[0] +  "&tmp=" + data[1] +  "&hum=" +data[2] )
+    print(data[0])
 
 ser.close()
--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-var express = require('express');
-var app = express();
+require('date-utils')
+const app = require("express")();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const moment = require('moment');
+moment().format();
+const fs = require('fs');
+const filename = 'sensorData.csv';
+const csv = require('csv');
 
-app.get('/', function (req, res) {
-  console.log(req.query.lux);
-  res.send();
+app.get('/putdata', function (req, res) {
+    let lux = req.query.lux;
+    let dt = new Date();
+    //console.log(lux);
+    req.query.time = dt;
+    let formatted  = dt.toFormat("YYYY-MMDD-HH24:MI.SS");
+    //console.log(formatted);
+    res.send();
+    const input = [formatted +',' + lux + '\n'];
+    csv.stringify(input, function(err, output){
+	console.log(req.query.lux);
+	console.log(req.query.tmp);
+	console.log(req.query.hum);
+	fs.appendFile(filename, output, function (err) {
+	    if (err) {
+		throw err;
+	    }
+	});
+    });
+    io.emit("send data",lux);
 });
 
-app.listen(15071, function () {
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket){
+  let dt = new Date();
+  let time = dt.toFormat("YYYY-MM-DD");
+  io.emit("first connect",filename);
+});
+
+http.listen(15071, function () {
   console.log('Example app listening on port 15071!');
 });
